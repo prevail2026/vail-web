a5616ee510e398bb1c838253a270f9cdbd2d40c4
vail <cantstopvoyage@gmail.com>
2026-08-24 16:00:46 -0500

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,11 +1,11 @@
 import { NextResponse } from "next/server";
 import { getSessionFromCookies } from "@/lib/session";
 
-export function middleware(request) {
+export async function middleware(request) {
   if (request.nextUrl.pathname.startsWith("/dashboard")) {
     if (request.nextUrl.pathname === "/dashboard/login") return NextResponse.next();
 
-    const session = getSessionFromCookies(request.headers.get("cookie"));
+    const session = await getSessionFromCookies(request.headers.get("cookie"));
     if (!session) {
       return NextResponse.redirect(new URL("/dashboard/login", request.url));
     }
